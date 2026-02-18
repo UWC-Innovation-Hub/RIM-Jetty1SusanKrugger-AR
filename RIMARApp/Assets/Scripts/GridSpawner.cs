9575b3d7d179d5d1873be97f006b3fd38ca33610
ShabnamArai <araishabnam@gmail.com>
2026-02-18 16:26:50 +0200

    Script tweaks for debugging (WIP)

--- RIMARApp/Assets/Scripts/GridSpawner.cs
+++ RIMARApp/Assets/Scripts/GridSpawner.cs
@@ -14,8 +14,11 @@ public class GridSpawner : MonoBehaviour
     [SerializeField] private float marquetteHeight; // 1 meter
     [SerializeField] private float cellSize; // 2.5cm
     [SerializeField] private float cubeHeight; // 0.002f
+    [SerializeField] private float qrSize; // 0.025f | 0.0125f for spawning at the corner of the qr code
 
-    private bool gridSpawned = false;
+    private GameObject currentGridParent; // parent all the cubes under one object so can delete them easily.
+
+    //private bool gridSpawned = false;
 
 
     private void OnEnable()
@@ -34,48 +37,71 @@ public class GridSpawner : MonoBehaviour
     {
         foreach (var trackedImage in args.added)
         {
-            if (!gridSpawned)
-            {
-                SpawnGridFromCorner(trackedImage);
-                gridSpawned = true;
-            }
+            SpawnGridFromCorner(trackedImage);
         }
     }
 
 
     void SpawnGridFromCorner(ARTrackedImage trackedImage)
     {
-        string qrName = trackedImage.referenceImage.name;
+        // Delete old grid if exists
+        if (currentGridParent != null)
+        {
+            Destroy(currentGridParent);
+        }
+
+        currentGridParent = new GameObject("GridParent");
 
         int columns = Mathf.RoundToInt(marquetteWidth / cellSize);
         int rows = Mathf.RoundToInt(marquetteHeight /  cellSize);
 
-        Vector3 startPosition = trackedImage.transform.position;
+        float halfQR = qrSize / 2f;
 
-        Color quadrantColor = Color.white;
+        Vector3 qrCenter = trackedImage.transform.position;
 
         Vector3 xDirection = trackedImage.transform.right;
         Vector3 zDirection = trackedImage.transform.forward;
 
+        string qrName = trackedImage.referenceImage.name;
+
+        // Determine correct directions and corner offset
+        Vector3 startCorner = qrCenter;
+
         if (qrName == "QR_TopLeft")
         {
-            quadrantColor = Color.red;
+            startCorner = qrCenter
+                - (xDirection * halfQR)
+                + (zDirection * halfQR);
+
+            //xDirection = xDirection;
+            zDirection = -zDirection;
         }
         else if (qrName == "QR_TopRight")
         {
-            quadrantColor = Color.green;
+            startCorner = qrCenter
+                + (xDirection * halfQR)
+                + (zDirection * halfQR);
+
             xDirection = -xDirection;
+            zDirection = -zDirection;
         }
         else if (qrName == "QR_BottomLeft")
         {
-            quadrantColor = Color.blue;
-            zDirection = -zDirection;
+            startCorner = qrCenter
+                - (xDirection * halfQR)
+                - (zDirection * halfQR);
+
+            //xDirection = xDirection;
+            //zDirection = zDirection;
         }
         else if (qrName == "QR_BottomRight")
         {
-            quadrantColor = Color.yellow;
+            startCorner = qrCenter
+                + (xDirection * halfQR)
+                - (zDirection * halfQR);
+
             xDirection = -xDirection;
-            zDirection = -zDirection;
+            //zDirection = zDirection;
         }
 
         for (int x = 0; x < columns; x++)
@@ -83,19 +109,37 @@ public class GridSpawner : MonoBehaviour
             for (int z =0; z < rows; z++)
             {
                 Vector3 spawnPosition =
-                    startPosition +
+                    startCorner +
                     (xDirection * x * cellSize) +
                     (zDirection * z * cellSize);
 
-                GameObject cube = Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+                GameObject cube = Instantiate(
+                    cubePrefab,
+                    spawnPosition,
+                    trackedImage.transform.rotation
+                );
 
-                cube.transform.localScale = new Vector3(cellSize, cubeHeight, cellSize);
+                cube.transform.localScale =
+                    new Vector3(cellSize, cubeHeight, cellSize);
 
-                if (x < columns / 2 && z < rows / 2)
-                {
-                    cube.GetComponent<Renderer>().material.color = quadrantColor;
-                }
+                cube.transform.parent = currentGridParent.transform;
+
+                // Colour all quadrants
+                bool leftSide = x < columns / 2;
+                bool bottomSide = z < rows / 2;
+
+                if (leftSide && !bottomSide)
+                    cube.GetComponent<Renderer>().material.color = Color.red;
+                else if (!leftSide && !bottomSide)
+                    cube.GetComponent<Renderer>().material.color = Color.green;
+                else if (leftSide && bottomSide)
+                    cube.GetComponent<Renderer>().material.color = Color.blue;
+                else
+                    cube.GetComponent<Renderer>().material.color = Color.yellow;
             }
         }
+
+        // Make grid stable by detaching from tracking updates
+        currentGridParent.transform.position = currentGridParent.transform.position;
     }
 }
