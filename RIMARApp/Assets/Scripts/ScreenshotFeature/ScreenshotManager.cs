340da0cbb2519b3ff222926a5cbdb3caa893b20c
ShabnamArai <araishabnam@gmail.com>
2026-03-24 13:49:13 +0200

    Screenshot Shutter and Audio implementation

--- RIMARApp/Assets/Scripts/ScreenshotFeature/ScreenshotManager.cs
+++ RIMARApp/Assets/Scripts/ScreenshotFeature/ScreenshotManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /*
@@ -12,10 +13,21 @@ public class ScreenshotManager : MonoBehaviour
 {
     public static ScreenshotManager Instance;
 
+    [Header ("Preview")]
     public GameObject screenshotPreviewPrefab;
+
+    [Header("Flash Effect")]
+    // Visual element to confirm screenshot has been taken
+    public Image flashImage;
+    [SerializeField] private float flashDuration; // 0.15f
+
+    [Header("Audio")]
+    // Audio element to confirm screenshot has been taken
+    public AudioSource shutterAudio;
 
     private List<Texture2D> screenshots = new List<Texture2D>();
 
+    [Header ("Double Tap Durations")]
     [SerializeField] private float lastTimeTap; // 0f
     [SerializeField] private float doubleTapThreshold; // 0.3f
 
@@ -91,6 +103,12 @@ public class ScreenshotManager : MonoBehaviour
 
     IEnumerator CaptureScreenshot()
     {
+        // Show and play the flash + sound first
+        StartCoroutine(FlashEffect());
+
+        if (shutterAudio != null)
+            shutterAudio.Play();
+        
         yield return new WaitForEndOfFrame();
 
         Texture2D screen = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
@@ -100,6 +118,34 @@ public class ScreenshotManager : MonoBehaviour
         screenshots.Add(screen);
 
         ShowPreview(screen);
+    }
+
+
+    IEnumerator FlashEffect()
+    {
+        if (flashImage == null) yield break;
+
+        // Fade IN (flash)
+        float t = 0;
+        while (t < flashDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Lerp(0, 1, t / flashDuration);
+            flashImage.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+
+        // Fade OUT (flash)
+        t = 0;
+        while (t < flashDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, t / flashDuration);
+            flashImage.color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+
+        flashImage.color = new Color(1, 1, 1, 0);
     }
 
 
